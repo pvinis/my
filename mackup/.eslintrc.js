15ac20ef14319e9a7dc32289c34b135d42cfaf64
Pavlos Vinieratos <pvinis@gmail.com>
2019-05-14 10:35:10 +0200

    add a few things

--- mackup/.eslintrc.js
+++ mackup/.eslintrc.js
@@ -1,26 +1,33 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true,
+    env: {
+        es6: true,
+        node: true,
     },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly',
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true,
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        'ecmaVersion': 2018,
-        'sourceType': 'module',
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    'plugins': ['react'],
-    'rules': {
-    'no-unused-vars': 'off',
-        'semi': ['error', 'never'],
-        'quotes': [2, 'single'   ],
+    plugins: ['react'],
+    rules: {
+        'no-unused-vars': 'off',
+        semi: ['error', 'never'],
+        quotes: ['error', 'single'],
         'comma-dangle': ['error', 'always-multiline'],
         'object-curly-spacing': ['error', 'always'],
+        'key-spacing': ['error'],
+        indent: ['error', 4],
+        'no-console': 'off',
+        'arrow-spacing': ['error'],
+        'no-multi-spaces': ['error', { 'ignoreEOLComments': true }], // has exceptions too, if needed
+
+        'react/jsx-indent': ['error', 4],
     },
 }
