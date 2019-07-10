52f5ecd65dedea29b6cc406c8186d1953b0731b8
Pavlos Vinieratos <pvinis@gmail.com>
2019-07-10 16:58:45 +0200

    more

--- mackup/.hammerspoon/init.lua
+++ mackup/.hammerspoon/init.lua
@@ -1,3 +1,5 @@
+-- toggle mic on google hangouts
+
 mute = false
 function muteHangouts()
 	previouslyFocusedWindow = hs.window.focusedWindow()
@@ -26,34 +28,22 @@ function muteHangouts()
 	previouslyFocusedWindow:focus()
 end
 
-meetMute = hs.menubar.new()
-flippedMute = false
-function setMeetMute(state)
-	if state then
-		if flippedMute then
-			meetMute:setTitle("BLABLA")
-		else
-			meetMute:setTitle("MUTED")
-		end
-	else
-		if flippedMute then
-			meetMute:setTitle("MUTED")
-		else
-			meetMute:setTitle("BLABLA")
-		end
-	end
-end
-
-function meetMuteClicked()
-	flippedMute = not flippedMute
-	setMeetMute(mute)
-end
-
-meetMute:setClickCallback(meetMuteClicked)
-setMeetMute(mute)
-
 hs.hotkey.bind({"cmd", "shift"}, "1", muteHangouts)
 
 
+-- put mac to screensaver
+
+function startScreenSaver()
+	start = [[
+		tell application "System Events"
+		start current screen saver
+	end tell
+	]]
+	hs.osascript.applescript(start)
+end
+
 bedtime = hs.menubar.new()
 bedtime:setIcon("./bedtime.pdf")
+bedtime:setClickCallback(startScreenSaver)
+
+hs.hotkey.bind({"cmd", "shift"}, "6", startScreenSaver)
